209151d146be0e3315c990f314176562eee9e758
tromao <tiagomromao@gmail.com>
2023-11-26 17:41:58 +0000

    Initial commit

---
+++ shared/src/androidMain/kotlin/com/example/myquizv1/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.example.myquizv1
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
